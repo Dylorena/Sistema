80b52ffe897eac3ad66884c99faedae5efcd2eb8
Dylorena <31518102+Dylorena@users.noreply.github.com>
2021-02-17 23:42:32 -0300

    Cria componente select

---
+++ src/components/select/select.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import _ from 'lodash';
+import { Attributes } from '../entity/entity';
+
+interface Props {
+  Attribute: Attributes;
+}
+
+class Select extends React.Component<Props, any> {
+  getOptions(source: string[]) {   
+    return _.map(source, (op, index) => {
+      return <option id={`op-${index}`} value={index} >{op}</option>
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <label
+          htmlFor={`${this.props.Attribute.Name}`}
+          style={{ color: this.props.Attribute.Required ? 'red' : '', width: '170px', textAlign: 'right' }}
+        >{this.props.Attribute.Description}</label>
+        <select
+          name={this.props.Attribute.Name}
+          id=""
+          style={{ width: this.props.Attribute.DisplayWidth }}
+          className="inputs">
+          {this.getOptions(this.props.Attribute.Source)}
+        </select>
+      </div>
+
+    );
+  }
+}
+
+export default Select;
